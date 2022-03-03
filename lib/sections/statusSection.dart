533520fa361f6307b6b8394a7a11c67657650cf4
Abin P S <abinps23@gmail.com>
2022-03-03 18:03:58 +0530

    Status Section Modified

--- lib/sections/statusSection.dart
+++ lib/sections/statusSection.dart
@@ -1,18 +1,12 @@
 import 'package:fb_ui_clone/assets.dart';
+import 'package:fb_ui_clone/widgets/avatar.dart';
 import 'package:flutter/material.dart';
 
 class StatusSection extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return ListTile(
-      leading: ClipRRect(
-        borderRadius: BorderRadius.circular(100),
-        child: Image.asset(
-          farhan,
-          height: 50,
-          width: 50,
-        ),
-      ),
+      leading: Avatar(displayImage: farhan),
       title: TextField(
         decoration: InputDecoration(
           hintText: "What's on your mind?",
